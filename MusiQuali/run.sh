40297e27e8cc14c4b6c0e2ef0dc6598ffa700d2f
“LazyToonG” <lionfortnite999@gmail.com>
2026-06-17 20:33:15 +0200

    jsp

--- MusiQuali/run.sh
+++ MusiQuali/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Configration MusiQuali"
+echo "Configuration MusiQuali"
 
 # 1. Setup
 bash setup.sh
@@ -10,7 +10,8 @@ source venv/bin/activate
 
 # 3. Lancement app
 echo "Démarrage Flask..."
-echo -e "\033[32mHTTPS : https://127.0.0.1\033[0m"
+# Modification ici : on ajoute le port 8000 dans l'affichage
+echo -e "\033[32mHTTPS : https://127.0.0.1:8000\033[0m"
 echo -e "\033[33mCtrl + clic pour ouvrir le lien\033[0m"
 echo -e "\033[33mCtrl + C pour arrêter le script\033[0m"
 
